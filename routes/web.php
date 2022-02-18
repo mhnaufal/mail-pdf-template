34265682a3771abf695efe62d66ca031ebf2aac8
mhnaufal <aal.pirates16@gmail.com>
2022-02-18 09:54:11 +0700

    update controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EmployeeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [EmployeeController::class, 'showEmployees']);
+Route::get('/employee/pdf', [EmployeeController::class, 'createPDF']);
